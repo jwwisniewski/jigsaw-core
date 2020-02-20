d6f563cdfdbaa5fe0bc148f5b21c3f3e044b3f9a
jwwisniewski <j.w.wisniewski@gmail.com>
2020-02-20 22:27:53 +0100

    add config and service provider

---
+++ config/jigsaw.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'availableClientLocales' => [
+        'pl' => 'polish',
+        'en' => 'english',
+        'de' => 'german',
+        'fr' => 'french',
+    ],
+    'defaultClientLocale' => 'pl'
+];
---
+++ src/JigsawCoreServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace jwwisniewski\Jigsaw\Subpage;
+
+use Illuminate\Support\ServiceProvider;
+
+class JigsawCoreServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/jigsaw.php' => config_path('jigsaw.php'),
+        ]);
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+    }
+
+}
